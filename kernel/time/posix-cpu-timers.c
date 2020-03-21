5587f8425b46c6d143de45910fa1f8fff917e93a
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-03-21 12:26:04 +0100

    lockdep: Add posixtimer context tracing bits
    Splitting run_posix_cpu_timers() into two parts is work in progress which
    is stuck on other entry code related problems. The heavy lifting which
    involves locking of sighand lock will be moved into task context so the
    necessary execution time is burdened on the task and not on interrupt
    context.
    
    Until this work completes lockdep with the spinlock nesting rules enabled
    would emit warnings for this known context.
    
    Prevent it by setting "->irq_config = 1" for the invocation of
    run_posix_cpu_timers() so lockdep does not complain when sighand lock is
    acquried. This will be removed once the split is completed.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200321113242.751182723@linutronix.de
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- kernel/time/posix-cpu-timers.c
+++ kernel/time/posix-cpu-timers.c
@@ -1155,8 +1155,11 @@ void run_posix_cpu_timers(struct task_struct *tsk)
 	if (!fastpath_timer_check(tsk))
 		return;
 
-	if (!lock_task_sighand(tsk, &flags))
+	lockdep_posixtimer_enter();
+	if (!lock_task_sighand(tsk, &flags)) {
+		lockdep_posixtimer_exit();
 		return;
+	}
 	/*
 	 * Here we take off tsk->signal->cpu_timers[N] and
 	 * tsk->cpu_timers[N] all the timers that are firing, and
@@ -1198,6 +1201,7 @@ void run_posix_cpu_timers(struct task_struct *tsk)
 			cpu_timer_fire(timer);
 		spin_unlock(&timer->it_lock);
 	}
+	lockdep_posixtimer_exit();
 }
 
 /*
