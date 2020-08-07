12ef3a8e464a4e4a176112e9d32b6a25de3a1bf2
Boqun Feng <boqun.feng@gmail.com>
2020-08-07 15:42:25 +0800

    lockdep: Introduce lock_list::dep
    To add recursive read locks into the dependency graph, we need to store
    the types of dependencies for the BFS later. There are four types of
    dependencies:
    
    *	Exclusive -> Non-recursive dependencies: EN
    	e.g. write_lock(prev) held and try to acquire write_lock(next)
    	or non-recursive read_lock(next), which can be represented as
    	"prev -(EN)-> next"
    
    *	Shared -> Non-recursive dependencies: SN
    	e.g. read_lock(prev) held and try to acquire write_lock(next) or
    	non-recursive read_lock(next), which can be represented as
    	"prev -(SN)-> next"
    
    *	Exclusive -> Recursive dependencies: ER
    	e.g. write_lock(prev) held and try to acquire recursive
    	read_lock(next), which can be represented as "prev -(ER)-> next"
    
    *	Shared -> Recursive dependencies: SR
    	e.g. read_lock(prev) held and try to acquire recursive
    	read_lock(next), which can be represented as "prev -(SR)-> next"
    
    So we use 4 bits for the presence of each type in lock_list::dep. Helper
    functions and macros are also introduced to convert a pair of locks into
    lock_list::dep bit and maintain the addition of different types of
    dependencies.
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200807074238.1632519-7-boqun.feng@gmail.com
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -214,6 +214,8 @@ struct lock_list {
 	struct lock_class		*links_to;
 	const struct lock_trace		*trace;
 	u16				distance;
+	/* bitmap of different dependencies from head to this */
+	u8				dep;
 
 	/*
 	 * The parent field is used to implement breadth-first search, and the
