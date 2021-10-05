40ab834641ba2f082ac15faf225db24967403bba
GtrCraft <nickvanbokhorst@hotmail.com>
2021-10-05 14:59:11 +0200

    build.sh: Cleanup & add new colors

--- build.sh
+++ build.sh
@@ -2,30 +2,26 @@
 
 #set -e
 
-## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=vendor/alioth_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 FINAL_KERNEL_ZIP=Optimus_Drunk_Alioth_v11.16.zip
 export PATH="$KERNELDIR/prebuilts/proton-clang/bin:${PATH}"
-export ARCH=arm64
-export SUBARCH=arm64
 export KBUILD_COMPILER_STRING="$($KERNELDIR/prebuilts/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
 # Speed up build process
 MAKE="./makeparallel"
 
 BUILD_START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
 nocol='\033[0m'
 
-# Clean build always lol
-echo "**** Cleaning ****"
+# Always do clean build lol
+echo -e "$yellow**** Cleaning ****$nocol"
 mkdir -p out
 make O=out clean
 
-echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$yellow**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****$nocol"
 echo -e "$blue***********************************************"
 echo "          BUILDING KERNEL          "
 echo -e "***********************************************$nocol"
@@ -40,28 +36,27 @@ make -j$(nproc --all) O=out \
                       OBJDUMP=llvm-objdump \
                       STRIP=llvm-strip
 
-echo "**** Verify Image.gz-dtb & dtbo.img ****"
+echo -e "$yellow**** Verify Image.gz-dtb & dtbo.img ****$nocol"
 ls $PWD/out/arch/arm64/boot/Image.gz-dtb
 ls $PWD/out/arch/arm64/boot/dtbo.img
 
-# Anykernel 3 time!!
-echo "**** Verifying AnyKernel3 Directory ****"
+echo -e "$yellow**** Verifying AnyKernel3 Directory ****$nocol"
 ls $ANYKERNEL3_DIR
-echo "**** Removing leftovers ****"
+echo -e "$yellow**** Removing leftovers ****$nocol"
 rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
 rm -rf $ANYKERNEL3_DIR/dtbo.img
 rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
 
-echo "**** Copying Image.gz-dtb & dtbo.img ****"
+echo -e "$yellow**** Copying Image.gz-dtb & dtbo.img ****$nocol"
 cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
 cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
 
-echo "**** Time to zip up! ****"
+echo -e "$yellow**** Time to zip up! ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
 cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $KERNELDIR/$FINAL_KERNEL_ZIP
 
-echo "**** Done, here is your sha1 ****"
+echo -e "$yellow**** Done, here is your checksum ****$nocol"
 cd ..
 rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
 rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
